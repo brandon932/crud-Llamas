6abd1bf8ac7a18bf763f3f9979b52a729ed6de49
brandon932 <bthornton_932@yahoo.com>
2015-10-19 15:19:30 -0600

    updated index and html file

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -3,3 +3,35 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
+
+$("form").on("submit", function(e){
+  e.preventDefault();
+  var llamaspitter;
+  if ($("#llamaspitter").is("checked")){
+    llamaspitter= true;
+  }else{
+    llamaspitter = false;
+  }
+  var payload = {
+    name: $("#llamaName").val(),
+    age: $("#llamaAge").val(),
+    spitter: llamaspitter
+  };
+  $.post("/api/llamas", payload, function(data){
+    $("#message").html("llama has been added!");
+  });
+  console.log(payload);
+});
+
+function getLlamas(){
+  $("allLlamas").html('');
+  $get("/api/llamas", function(data){
+    for (var i = 0; i < data.length; i++) {
+      $("#allLlamas").prepend(
+        '<tr><td>'+ data[i].name + "</td></tr>"+
+        '<tr><td>'+ data[i].age + "</td></tr>"+
+        '<tr><td>'+ data[i].spitter+ "</td></tr>"
+      );
+    }
+  });
+}
